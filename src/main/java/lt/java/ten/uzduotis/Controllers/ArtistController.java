28d59b466ae0d1aae06c1cf375f33eea72b2c39b
cipherswan <leskauskas25@gmail.com>
2017-12-14 21:43:59 +0200

    Update

--- src/main/java/lt/java/ten/uzduotis/Controllers/ArtistController.java
+++ src/main/java/lt/java/ten/uzduotis/Controllers/ArtistController.java
@@ -1,11 +1,14 @@
 package lt.java.ten.uzduotis.Controllers;
 
+import lt.java.ten.uzduotis.Entities.Artist;
 import lt.java.ten.uzduotis.Services.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ArtistController {
@@ -14,6 +17,38 @@ public class ArtistController {
     @Autowired
     public void setArtistService(ArtistService artistService){
         this.artistService = artistService;
+    }
+
+
+    @RequestMapping("artist/new")
+    public String newArtist(Model model){
+        model.addAttribute("artist", new Artist());
+        return "artistform";
+    }
+
+
+    @RequestMapping(value = "artist", method = RequestMethod.POST)
+    public String saveArtist(Artist artist){
+        artistService.saveArtist(artist);
+        return "redirect:/artist/" + artist.getId();
+    }
+
+    @RequestMapping("artist/{id}")
+    public String showArtist(@PathVariable Integer id, Model model){
+        model.addAttribute("artist", artistService.artistById(id));
+        return "artistshow";
+    }
+
+    @RequestMapping("artist/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        model.addAttribute("artist", artistService.artistById(id));
+        return "artistform";
+    }
+
+    @RequestMapping("artist/delete/{id}")
+    public String delete(@PathVariable Integer id){
+        artistService.deleteArtist(id);
+        return "redirect:/artists";
     }
 
     @RequestMapping(value = "/artists", method = RequestMethod.GET)
--- src/main/java/lt/java/ten/uzduotis/Controllers/IndexController.java
+++ src/main/java/lt/java/ten/uzduotis/Controllers/IndexController.java
@@ -1,11 +1,17 @@
 package lt.java.ten.uzduotis.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class IndexController implements ErrorController {
+
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
 
     private static final String PATH = "/error";
 
@@ -20,7 +26,9 @@ public class IndexController implements ErrorController {
     }
 
     @RequestMapping("/")
-    String index(){
-        return "index";
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
     }
+
 }
