2f5ab0784eac502ae7e957446b8e4e2d60fe9516
cipherswan <leskauskas25@gmail.com>
2017-12-17 13:00:24 +0200

    Add Google Auth

---
+++ src/main/java/lt/java/ten/uzduotis/Configuration/WebSecurityConfiguration.java
@@ -0,0 +1,29 @@
+package lt.java.ten.uzduotis.Configuration;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableOAuth2Sso
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf()
+                .disable()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/welcome.html")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+      //  super.configure(http);
+    }
+}
